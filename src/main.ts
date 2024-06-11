bd22c1232a1c69b387de6580d0e1679e55401195
Kreme201 <kreme201@gmail.com>
2024-06-12 00:42:23 +0900

    add main scene

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
-window.onload = () => {
-  console.log('Hello World');
-}
+import { Game } from 'phaser';
+import { Main } from './scenes/Main';
+
+new Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 300 },
+            debug: false
+        }
+    },
+    scene: Main,
+});
